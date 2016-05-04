06c8b1cc0c9da4c9d7808f3e286e75302d720a65
rainbeam <rainbeam@riseup.net>
2016-05-04 18:06:03 +0100

    bitcoin transaction parsing library stub

---
+++ contracts/btc_tx.sol
@@ -0,0 +1,46 @@
+// Bitcoin transaction parsing library
+
+// https://en.bitcoin.it/wiki/Protocol_documentation#tx
+//
+// Raw Bitcoin transaction structure:
+//
+// field     | size | type     | description
+// version   | 4    | int32    | transaction version number
+// n_tx_in   | 1+   | var_int  | number of transaction inputs
+// tx_in     | 41+  | tx_in[]  | list of transaction inputs
+// n_tx_out  | 1+   | var_int  | number of transaction outputs
+// tx_out    | 9+   | tx_out[] | list of transaction outputs
+// lock_time | 4    | uint32   | block number / timestamp at which tx locked
+//
+// Transaction input (tx_in) structure:
+//
+// field      | size | type     | description
+// previous   | 36   | outpoint | Previous output transaction reference
+// script_len | 1+   | var_int  | Length of the signature script
+// sig_script | ?    | uchar[]  | Script for confirming transaction authorization
+// sequence   | 4    | uint32   | Sender transaction version
+//
+// OutPoint structure:
+//
+// field      | size | type     | description
+// hash       | 32   | char[32] | The hash of the referenced transaction
+// index      | 4    | uint32   | The index of this output in the referenced transaction
+//
+// Transaction output (tx_out) structure:
+//
+// field         | size | type     | description
+// value         | 8    | int64    | Transaction value (Satoshis)
+// pk_script_len | 1+   | var_int  | Length of the public key script
+// pk_script     | ?    | uchar[]  | Public key as a Bitcoin script.
+//
+// Variable integers (var_int) can be encoded differently depending
+// on the represented value, to save space. Variable integers always
+// precede an array of a variable length data type (e.g. tx_in).
+//
+// Variable integer encodings as a function of represented value:
+//
+// value           | storage length (hex)  | format
+// <0xFD (253)     | 1                     | uint8
+// <=0xFFFF (65535)| 3                     | 0xFD followed by length as uint16
+// <=0xFFFF FFFF   | 5                     | 0xFE followed by length as uint32
+// -               | 9                     | 0xFF followed by length as uint64
