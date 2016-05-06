f82433eb6a0aa911d2f09063100bb48c797fafca
rainbeam <rainbeam@riseup.net>
2016-05-06 14:19:30 +0100

    consistent test variable names

--- contracts/btc_tx_test.sol
+++ contracts/btc_tx_test.sol
@@ -36,21 +36,21 @@ contract BTCTxTest is Test {
     function testParseVarInt8() {
         bytes memory data = new bytes(1);
         data[0] = 0x00;
-        var (ret, pos) = BTC.parseVarInt(data, 0);
-        assertEq(ret, 0);
+        var (val, pos) = BTC.parseVarInt(data, 0);
+        assertEq(val, 0);
         assertEq(pos, 1);
 
         data[0] = 0xfc;
-        (ret, pos) = BTC.parseVarInt(data, 0);
-        assertEq(ret, 252);
+        (val, pos) = BTC.parseVarInt(data, 0);
+        assertEq(val, 252);
     }
     function testParseVarInt16() {
         bytes memory data = new bytes(3);
         data[0] = 0xfd;
         data[1] = 0x00;
         data[2] = 0x00;
-        var (ret, pos) = BTC.parseVarInt(data, 0);
-        assertEq(ret, 0);
+        var (val, pos) = BTC.parseVarInt(data, 0);
+        assertEq(val, 0);
         assertEq(pos, 3);
     }
 }
